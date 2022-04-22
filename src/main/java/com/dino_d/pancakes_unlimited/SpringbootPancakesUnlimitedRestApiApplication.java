0cc4f08d02763325c42a0570bdc8ab7c0683932b
DinoDamjanovic <ddamjanov@student.unizd.hr>
2022-04-22 13:32:23 +0200

    Initial commit

---
+++ src/main/java/com/dino_d/pancakes_unlimited/SpringbootPancakesUnlimitedRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.dino_d.pancakes_unlimited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPancakesUnlimitedRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPancakesUnlimitedRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dino_d/pancakes_unlimited/SpringbootPancakesUnlimitedRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dino_d.pancakes_unlimited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPancakesUnlimitedRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
